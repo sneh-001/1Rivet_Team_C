cc3f47a7c9bccafef2ab712a9cbb75c915c97ba6
Sneh <snehsoni001@gmail.com>
2025-01-22 16:27:21 +0530

    Delete CalculatorApp directory

--- CalculatorApp/src/main/java/com/example/calculator/Main.java
+++
@@ -1,8 +0,0 @@
-package com.example.calculator;
-
-public class Main {
-    public static void main(String[] args) {
-        // This main method can be used for testing or additional logic if needed.
-        System.out.println("Calculator Application is running.");
-    }
-}
